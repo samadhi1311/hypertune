8198c810ecc648dfa5e5822ab5cb6017bdd89de5
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-04-24 20:02:50 +0530

    feat: Added window action buttons

--- src/components/title-bar.tsx
+++ src/components/title-bar.tsx
@@ -1,6 +1,7 @@
 import { getCurrentWindow } from '@tauri-apps/api/window';
 import { Button } from './ui/button';
 import { useEffect } from 'react';
+import { MinusIcon, SquareIcon, XIcon } from 'lucide-react';
 
 export default function TitleBar() {
 	useEffect(() => {
@@ -10,14 +11,34 @@ export default function TitleBar() {
 			appWindow.minimize();
 		});
 
+		document.getElementById('titlebar-maximize')?.addEventListener('click', () => {
+			appWindow.toggleMaximize();
+		});
+
 		document.getElementById('titlebar-close')?.addEventListener('click', () => {
 			appWindow.close();
 		});
 	}, []);
 	return (
-		<header data-tauri-drag-region className='flex flex-row items-center justify-end'>
-			<Button id='titlebar-minimize'>Minimize</Button>
-			<Button id='titlebar-close'>Close</Button>
+		<header
+			data-tauri-drag-region
+			className='flex flex-row items-center justify-between gap-4 bg-sidebar/80 rounded-md border border-border inset-shadow-[0px_0px_32px] inset-shadow-border/50 backdrop-blur-xl p-2'>
+			<div className='flex items-center gap-1 pl-2 text-muted-foreground'>
+				<img src='hypertune.svg' className='size-8 aspect-square' />
+				<img src='hypertune-wordmark.svg' className='h-8 opacity-80' />
+			</div>
+
+			<div className='flex gap-2'>
+				<Button className='size-7' variant='ghost' id='titlebar-minimize'>
+					<MinusIcon strokeWidth={2} className='text-muted-foreground' />
+				</Button>
+				<Button className='size-7' variant='ghost' id='titlebar-maximize'>
+					<SquareIcon strokeWidth={2} className='size-3 text-muted-foreground' />
+				</Button>
+				<Button className='size-7' variant='ghost' id='titlebar-close'>
+					<XIcon strokeWidth={2} className='text-muted-foreground' />
+				</Button>
+			</div>
 		</header>
 	);
 }
